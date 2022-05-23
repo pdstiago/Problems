521766f20b7bf7569d4952b06e3095330e039a6d
Tiago Silva <tiago.pds.silva@gmail.com>
2022-05-23 09:58:56 -0300

    Update ufrj.c

--- ufrj.c
+++ ufrj.c
@@ -157,3 +157,39 @@ int main(){
     printf("O maior valor da lista é %d\nO menor valor da lista é %d\nA quantidade de valores pares é %d\nA quantidade de valores ímpares é %d\n", maior, menor, par, impar);
     return 0;
 }
+--------------------------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+ 
+int main(){
+    int matriz1[5][5], matriz2[5][5], mult[5][5];
+    printf("Insira a primeira matriz:\n");
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            scanf("%d", &matriz1[i][j]);
+        }
+    }
+    printf("\nInsira a segunda matriz:\n");
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            scanf("%d", &matriz2[i][j]);
+        }
+    }
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            int soma=0;
+            for(int k=0; k<5; k++){
+                soma+=matriz1[i][k]*matriz2[k][j];
+            }
+            mult[i][j]=sum;
+            soma=0;
+        }
+    }
+    printf("A multiplicação das duas matrizes é igual a\n");
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            printf("%d ", mult[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
