b679805630c8b200144348939e89780fd646241e
Tiago Silva <tiago.pds.silva@gmail.com>
2022-08-09 04:54:16 -0300

    Update CurrentProblem.cpp

--- CurrentProblem.cpp
+++ CurrentProblem.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define MOD 1000000009
-#define mxn 100005
-#define mxm 505
+#define mod 100005
+#define mxn 200005
+#define mxm 1005
 #define f first
 #define s second
 #define pb push_back
@@ -11,78 +12,51 @@ using namespace std;
 #define endl '\n'
 #define INF 0x3f3f3f3f
 #define INFL 0x3f3f3f3f3f3f3f3f
-#define ll long long
+#define ll long long int
 #define fastio ios_base::sync_with_stdio(0), cin.tie(0)
 #define sz(x) (int)x.size()
 #define all(x) x.begin(), x.end()
-typedef pair<string, int> pii;
+typedef pair<int, int> pii;
 typedef pair<int, pii> pip;
 
-int m[5][5], soma, qntl[5][2], qntc[5][2], qntd[2][2]; 
+int n, m, k, p, cont, parei;
+ll atual=1;
+string senha, got="", res="";
+vector<char> prob[28];
 
 int main(){
-    fastio;
-    for(int i=1; i<=3; i++){
-        for(int j=1; j<=3; j++){
-            cin >> m[i][j];
+    cin >> n >> m >> k >> senha;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<k; j++){
+            char a;
+            cin >> a;
+            prob[i].pb(a);
         }
     }
-    for(int i=1; i<=3; i++){
-        for(int j=1; j<=3; j++){
-            if(m[i][j]){
-                qntl[i][1]++;
-            }
-            qntl[i][0]+=m[i][j];
+    cin >> p;
+    p--;
+    for(int i=m-1; i>=0; i--){
+        atual*=k;
+        got+=prob[i][p%k];
+        p-=(atual-1);
+        if(p<=0){
+            parei=i;
+            break;
         }
     }
-    for(int i=1; i<=3; i++){
-        for(int j=1; j<=3; j++){
-            if(m[j][i]){
-                qntc[i][1]++;
-            }
-            qntc[i][0]+=m[j][i];
+    cout << got << endl;
+    for(int i=parei-1; i>=0; i--){
+        got+=prob[i][0];
+    }
+    for(int i=n-1; i>=0; i--){
+        if(senha[i]=='#'){
+            res+=got[cont++];
+        }else{
+            res+=senha[i];
         }
     }
-    for(int i=1; i<=3; i++){
-        if(m[i][i]){
-            qntd[0][1]++;
-        }
-        qntd[0][0]+=m[i][i];
+    for(int i=n-1; i>=0; i--){
+        cout << res[i];
     }
-    int j=0;
-    for(int i=3; i>=1; i--){
-        j++;
-        if(m[j][i]){
-            qntd[1][1]++;
-        }
-        qntd[1][0]+=m[j][i];
-    }
-    for(int i=1; i<=3; i++){
-        if(qntl[i][1]==3){
-            soma=qntl[i][0];
-        }
-    }
-    for(int i=1; i<=3; i++){
-        if(qntc[i][1]==3){
-            soma=qntc[i][0];
-        }
-    }
-    for(int i=0; i<2; i++){
-        if(qntd[i][1]==3){
-            soma=qntd[i][0];
-        }
-    }
-    for(int i=1; i<=3; i++){
-        if(qntl[i][1]==2){
-            for(int j=1; j<=3; j++){
-                if(!m[i][j]){
-                    m[i][j]=soma-qntl[i][0];
-                }
-            }
-            qntl[i][1]++;
-            qntl[i][0]=soma;
-        }
-    }
-    cout << realsoma << endl;
     return 0;
 }
